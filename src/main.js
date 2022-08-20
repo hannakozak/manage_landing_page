54bd099e3148a0ea192dba0a6ac4c34e6f853548
Hanna Kozak <hannawkozak@gmail.com>
2022-08-20 22:15:23 +0100

    feat: add header with navigation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
+/* eslint-disable import/no-absolute-path */
+// eslint-disable-next-line import/no-unresolved
 import "/scss/main.scss";
+
+const navigationIcon = document.querySelector(".navigation__icon");
+const iconClose = document.querySelector(".navigation__icon--close");
+const iconOpen = document.querySelector(".navigation__icon--open");
+const navigation = document.querySelector(".navigation__nav");
+const overlay = document.querySelector(".navigation__overlay");
+
+function toggleMenu() {
+  overlay.classList.toggle("overlay");
+  if (navigation.classList.contains("showMenu")) {
+    navigation.classList.remove("showMenu");
+    iconClose.style.display = "none";
+    iconOpen.style.display = "block";
+  } else {
+    navigation.classList.add("showMenu");
+    iconClose.style.display = "block";
+    iconOpen.style.display = "none";
+  }
+}
+
+navigationIcon.addEventListener("click", toggleMenu);
